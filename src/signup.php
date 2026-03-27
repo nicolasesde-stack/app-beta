f110248c2851bc5660cbbae370af0ccc7328180c
Nicolas <nicolas.esde@gmail.com>
2026-03-27 12:15:32 -0500

    primer punto

--- src/signup.php
+++ src/signup.php
@@ -11,4 +11,14 @@
     $sql="INSERT INTO users(firstname,lastname,email,mobile_phone,password)
         values('$f_name','$l_name','$e_mail','$m_phone','$p_sswd')";
     //Execute query
-    pg_query($sql);    
+    pg_query($sql);  
+    
+    //email 
+$check_email = "SELECT email FROM users WHERE email = '$e_mail'";
+$res_email = pg_query($local_conn, $check_email);
+
+if (pg_num_rows($res_email) > 0) {
+    echo "Error: El correo electrónico '$e_mail' ya está registrado. Por favor, use uno diferente.\n";
+    exit();
+}
+
